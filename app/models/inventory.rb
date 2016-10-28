1bf922ad9d1570dca66e791f50aecd48ed03fa6c
Rodrigo Vega <jr_veg@hotmail.com>
2016-10-27 22:23:22 -0500

    commit 9

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,16 +1,24 @@
 class Inventory < ActiveRecord::Base
+  after_destroy :msgDelete
+
   belongs_to :category
-  has_one :work, dependent: :delate_all
+  has_one :work, dependent: :delete
   validate :valid_size
 
   scope :tam_x, ->(x){where(:size =>x)}
-  
+
 
   def valid_size
     if self.size.nil? || self.size <= 0
       errors.add(:size, "it must be greather than 0")
     end
   end
+
+def msgDelete
+  if
+    Log.create(description: "La pieza con serial #{self.serial} ha sido borrado")
+  end
+end
 
   # validates :size, numericality: { greater_than: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028015231) do
+ActiveRecord::Schema.define(version: 20161028030455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 20161028015231) do
   end
 
   add_index "inventories", ["category_id"], name: "index_inventories_on_category_id", using: :btree
+
+  create_table "logs", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
